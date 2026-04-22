719640e873a63a146e44376d53a00a2e0887a503
Sanjai Lashkarai <sanjailaskarai@gmail.com>
2026-04-22 12:08:42 +0530

    Delete Web Projects directory

--- Web Projects/Stock App/fullStock.js
+++
@@ -1,8 +0,0 @@
-export default function getStockData() {
-  return {
-    name: "QtechAI",
-    symbol: "QTA",
-    price: +(Math.random() * (3)).toFixed(2),
-    time: new Date().toLocaleTimeString()
-  };
-}
